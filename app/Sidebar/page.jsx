eb5e4c2ac561198a6751279f6ca7f42ae442a356
Diamela <diamelavlioi@gmail.com>
2024-03-13 14:05:50 -0300

    global.css

--- app/Sidebar/page.jsx
+++ app/Sidebar/page.jsx
@@ -30,57 +30,51 @@ const Sidebar = () => {
 
 
   return (
-    // <motion.div
-    // initial={{ scale: 1}}
-    // transition={{duration: 2}}
-    // animate={{scale: 2}}>
-      <motion.div 
-        initial={{ scale: -2}}
-    transition={{duration: 4}}
-    animate={{scale: 1}}
+    <motion.div
+      initial={{ scale: -2 }}
+      transition={{ duration: 4 }}
+      animate={{ scale: 1 }}
       className={styles.container}>
-        <Link href='/Home' onClick={(e) => scrollToSection(e, 'Home')}>
-          <div
-            className={styles.circle}>
-            <FaHome className={styles.icon} />
-            <span className={styles.span}>Portfolio</span>
-          </div>
+      <Link href='/Home' onClick={(e) => scrollToSection(e, 'Home')}>
+        <div
+          className={styles.circle}>
+          <FaHome className={styles.icon} />
+          <span className={styles.span}>Portfolio</span>
+        </div>
+      </Link>
+      <Link className={styles.links} href='/About' onClick={(e) => scrollToSection(e, 'About')}>
+        <div className={styles.circle}>
+          <PiPersonArmsSpreadFill className={styles.icon} />
+          <span className={styles.span}>Sobre Mí</span>
+        </div>
+      </Link>
+      <Link className={styles.links} href='/Portfolio' onClick={(e) => scrollToSection(e, 'Portfolio')}>
+        <div className={styles.circle}>
+          <AiFillDatabase className={styles.icon} />
+          <span className={styles.span}>Portfolio</span>
+        </div>
+      </Link>
+      <div className={styles.circle}>
+        <Link className={styles.links} href='/Cv' onClick={(e) => scrollToSection(e, 'Cv')}><IoDocumentText className={styles.icon} />
+          <span className={styles.span}>Curriculum</span>
         </Link>
-        <Link className={styles.links} href='/About' onClick={(e) => scrollToSection(e, 'About')}>
-          <div className={styles.circle}>
-            <PiPersonArmsSpreadFill className={styles.icon} />
-            <span className={styles.span}>Sobre Mí</span>
-          </div>
+      </div>
+      <div className={styles.circle}>
+        <Link className={styles.links} href='/Service' onClick={(e) => scrollToSection(e, 'Service')}><GrServices className={styles.icon} />
+          <span className={styles.span}>Servicios</span>
         </Link>
-        <Link className={styles.links} href='/Portfolio' onClick={(e) => scrollToSection(e, 'Portfolio')}>
-          <div className={styles.circle}>
-            <AiFillDatabase className={styles.icon} />
-            <span className={styles.span}>Portfolio</span>
-          </div>
+      </div>
+      <div className={styles.circle}>
+        <Link className={styles.links} href='/Sikills' onClick={(e) => scrollToSection(e, 'Skills')}><GiSkills className={styles.icon} />
+          <span className={styles.span}>Habilidades</span>
         </Link>
-        <div className={styles.circle}>
-          <Link className={styles.links} href='/Cv' onClick={(e) => scrollToSection(e, 'Cv')}><IoDocumentText className={styles.icon} />
-            <span className={styles.span}>Curriculum</span>
-          </Link>
-        </div>
-        <div className={styles.circle}>
-          <Link className={styles.links} href='/Service' onClick={(e) => scrollToSection(e, 'Service')}><GrServices className={styles.icon} />
-            <span className={styles.span}>Servicios</span>
-          </Link>
-        </div>
-        <div className={styles.circle}>
-          <Link className={styles.links} href='/Sikills' onClick={(e) => scrollToSection(e, 'Skills')}><GiSkills className={styles.icon} />
-            <span className={styles.span}>Habilidades</span>
-
-          </Link>
-        </div>
-        <div className={styles.circle}>
-          <Link className={styles.links} href='/Contact' onClick={(e) => scrollToSection(e, 'Contact')}><IoMailUnreadSharp className={styles.icon} />
-            <motion.span className={styles.span}>Contacto</motion.span>
-          </Link>
-        </div>
-      </motion.div>
-    // </motion.div>
+      </div>
+      <div className={styles.circle}>
+        <Link className={styles.links} href='/Contact' onClick={(e) => scrollToSection(e, 'Contact')}><IoMailUnreadSharp className={styles.icon} />
+          <motion.span className={styles.span}>Contacto</motion.span>
+        </Link>
+      </div>
+    </motion.div>
   )
 }
 
